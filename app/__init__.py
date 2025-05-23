e6175a3dfbf570b640bd10fa36546f804ede2f12
Vladimir Myasnikov <darkoptimist1999@gmail.com>
2025-05-23 19:30:18 +0300

    New versions of libraries

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from keras.models import load_model
+#from tensorflow.python.keras.models import load_model
+
 import numpy as np
 import pickle
 from sklearn.pipeline import Pipeline
@@ -7,6 +9,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
 app = Flask(__name__)
-matModel = load_model('models/matmod')
+matModel = load_model('models/matmod/matmod.keras')
 pprModel = pickle.load(open("models/ppr/model_ppr.pickle", "rb"))
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, matModel, pprModel
 from flask import render_template, request
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
 
 @app.route('/')
 @app.route('/index')
@@ -11,7 +12,7 @@ def index():
 def predict():
     print(list(request.form.values()))
     features = np.array([float(i) for i in request.form.values()])
-    predictmat = round(matModel.predict(features)[0][0], 2)
+    predictmat = round(matModel.predict(features.reshape(1,-1))[0][0], 2)
     predictppr = pprModel.predict(features.reshape(1,-1))[0]
     return render_template('index.html',
                            prediction_text="Рекомендуемое соотношение матрица-наполнитель: {:.2f}\n" \
